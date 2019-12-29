088cd106eec31576e518e7564416f0d08d8850e5
Anhquan075 <54442768+anhquan075@users.noreply.github.com>
2019-12-29 23:18:48 +0700

    Update Exercise 5

--- Introduction to Programming/Exercise 5/352.cpp
+++ Introduction to Programming/Exercise 5/352.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 
 
 void Import_Matrix(int arr[][100],int n,int m)
@@ -27,27 +27,37 @@ void Export_Matrix(int arr[][100],int n,int m)
 	}
 }
 
-int Check_Any_Pos_Num(int arr[][100],int n,int m)
+
+
+int Check_Rows_Up(int arr[][100],int x,int n)
 {
 	int flag = 0;
-	for (int i = 0; i < n; i++)
+	for(int j = 0;j < n - 1;j++)
 	{
-		for(int j = 0;j < m;j++)
-		 {
-		 	if(arr[i][j] > 0)
-		 		flag = 1;
-		 }
+		for(int k = j + 1;k < n;k++)
+		{
+			if(arr[x][j] < arr[x][k])
+				flag = 1;
+		}
 	}
 	return flag;
 }
 
-int main()
-{
+int main(){
 	int arr[100][100];
-	int n,m;
+	int n,m,x;
 	cin>>n>>m;
 	Import_Matrix(arr,n,m);
 	Export_Matrix(arr,n,m);
-	cout<<Check_Any_Pos_Num(arr,n,m);
+	do
+	{
+		cin>>x;
+		if(x < 0 || x > n -1)
+			cout<<"Vui long nhap lai !";
+	}while(x < 0 || x > n -1);
+	if(Check_Rows_Up(arr,x,n) == 1)
+		cout<<"Dong "<<x<<" tang dan !";
+	else
+		cout<<"Dong "<<x<<" khong tang dan !";
 	return 0;
 }
